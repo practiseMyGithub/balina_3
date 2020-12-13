8ed54542c2d032451194b21702248610fb80a1f1
williams <joniehh@yahoo.de>
2020-12-13 04:38:20 +0100

    add Post page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
+import Post from './components/pages/Post'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/posts" component={Post} />
       </Switch>
     </Router>
   );
