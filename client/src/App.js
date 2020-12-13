18c4732e8dc3c565bb7555304f391b7d752cf806
williams <joniehh@yahoo.de>
2020-12-13 04:47:44 +0100

    add expense Page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Post from './components/pages/Post'
+import Expense from './components/pages/Post'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/posts" component={Post} />
+        <Route exact path="/expenses" component={Expense} />
       </Switch>
     </Router>
   );
