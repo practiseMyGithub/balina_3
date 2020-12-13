14c9d72524ec92a7d558d53b89d6bf61f2670fef
williams <joniehh@yahoo.de>
2020-12-13 05:32:37 +0100

    test routes

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/expenses">Expenses</Link>
     </div>
   )
 }
