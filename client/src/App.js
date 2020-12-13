87622bf093f0826f81d1c48071a578e2c4675f8c
williams <joniehh@yahoo.de>
2020-12-13 04:15:09 +0100

    add page Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      Hello world"
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
