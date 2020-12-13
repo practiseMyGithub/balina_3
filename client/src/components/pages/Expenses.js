469b6c6222c81a1b83cc7040357b9af4c7b9b998
williams <joniehh@yahoo.de>
2020-12-13 06:56:03 +0100

    PostForm & Expense index

--- client/src/components/pages/Expenses.js
+++ client/src/components/pages/Expenses.js
@@ -1,11 +1,107 @@
 import React from 'react'
 
 const Expenses = () => {
+
+  // FOR MONTH SELECT OPTION
+  const expenseMonth = () => {
+    const months = 'January,February,March,April,May,June,July,August,September,October,November,December';
+    const monthsArr = months.split(',');
+    return monthsArr;
+  }
+
+  // FOR YEAR SELECT OPTION 
+  const expenseYear = () => {
+    let elements = [];
+    const thisYear = new Date().getFullYear();
+
+    for (let i = 0; i <= 5; i++) {
+      elements.push(thisYear + i);
+    }
+
+    return elements;
+  }
+
   return (
     <div className="row">
       <div className="col-md">
         <div className="content">
-          Expenses
+          <div style={{ position: "relative", margin: 'auto' }}>
+            {/* <Alert /> */}
+          </div>
+          <div className="row">
+            {/* <!-- EXPENSE FILTER FORM --> */}
+            <div className="col-md-6">
+              <form id="expense-filter-form" className="form-group form-inline">
+                <select id="month-expenses" className="form-control mr-2">
+
+                  {/* <option>-- Select Month --</option> */}
+                  {expenseMonth().length > 0 && expenseMonth().map((month, i) => (
+                    <option key={i + 1} value={i + 1} >
+                      {month}
+                    </option>
+                  ))}
+                </select>
+                <select className="form-control mr-2">
+                  {/* <option>--Select Year--</option> */}
+                  {expenseYear().map(exp => (
+                    <option key={exp} value={exp && exp}>{exp}</option>
+                  ))}
+                </select>
+                <button type="submit" className="btn btn-success"><i className="fas fa-search"></i></button>
+              </form>
+            </div>
+            {/* <!-- total expense --> */}
+            <div className="col-md-2">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">฿</span>
+                </div>
+                {/* calculateExpense('expense') */}
+                <input type="text" className="form-control text-danger" aria-label="Amount (to the nearest bath)" disabled />
+              </div>
+            </div>
+            {/* <!-- total income --> */}
+            <div className="col-md-2">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">฿</span>
+                </div>
+                <input type="text" className="form-control" aria-label="Amount (to the nearest bath)" disabled />
+              </div>
+            </div>
+            {/* <!-- add expense button --> */}
+            {/* {user && user.role !== 'member' && ( */}
+            <div className="col-md-2">
+              <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#expenseFormModal">
+                <i className="fas fa-plus"></i> Add Expense
+              </button>
+            </div>
+            {/* )} */}
+          </div>
+
+          {/* Expense item */}
+          <div className="row">
+            <div className="table-responsive">
+              <table id="expense-table" className="table table-striped table-sm">
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Subject</th>
+                    <th>Title</th>
+                    <th>Company</th>
+                    <th>Note</th>
+                    <th>Amount</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* {expenses && expenses.map(expense => (
+                  <ExpenseItem key={expense._id} expense={expense} />
+                ))} */}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/post/PostForm.js
+++ client/src/components/post/PostForm.js
@@ -1,12 +1,82 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
+
 
 const PostForm = () => {
   return (
     <div className="row">
       <div className="col-md">
         <div className="content">
+          <div className="container w-50" >
+            <div className="card">
+              <div className="card-header">
+                <h3>New Post</h3>
+              </div>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-12">
+                    <form id="add-post-form" className="form-group">
+                      <div className="mb-3">
+                        <small>Title:</small>
+                        <input type="text" name="title" id="title" className="form-control" />
+                      </div>
+                      <div className="mb-3">
+                        <small>Category</small>
+                        <select id="category-post" name="category" className="form-control">
+                          <option value="" className="category">..... .... .....</option>
+                          <option value="Garden" className="category">Garden</option>
+                          <option value="Pool" className="category">Pool</option>
+                          <option value="Internet" className="category">Internet</option>
+                          <option value="Fitness" className="category">Fitness</option>
+                          <option value="Legal" className="category">Legal</option>
+                          <option value="Bank" className="category">Bank</option>
+                          <option value="Others" className="category">Others</option>
+                        </select>
+                      </div>
+                      <div className="custom-file mb-2">
+                        {/* {user && user.role !== 'member' && ( */}
+                        <>
+                          <input type="checkbox" name="status" className="status mr-2" /><small>Edit voting</small>
+                        </>
+                        {/* )} */}
+                        <input type="checkbox" name="complain" className="complain ml-4 mr-2" /><small>complain ??</small>
+                      </div>
 
-          Post form
+                      {/* Choose date/option for vote */}
+                      {/* {!activateVote && activateVote || status && ( */} {/*OLD*/}
+                      {/* {activateVote && ( */}
+                      <div className="mb-3">
+                        <div className="mb-3">
+                          {/* option vote */}
+
+                          <input type="radio" name="optionVote" className="option mr-2" />
+                          <small>No option</small>
+
+                          <input type="radio" name="optionVote" className="option ml-4 mr-2" />
+                          <small>With option (max. 4)</small>
+
+                        </div>
+                        {/* calender */}
+                        <small className="mr-3">End time:</small> <Moment format='DD/MM/YYYY' className="text-danger">0000</Moment> <br />
+                        <input type="date" name="finalVote" className="my-3" />
+                      </div>
+                      {/* )} */}
+
+                      <div>
+                        <small>Description:</small>
+                        <textarea name="description" id="description" className="form-control" cols="20" rows="10"></textarea>
+                      </div>
+                      <div className="float-right mt-4">
+                        <input type="submit" className="btn btn-primary" />{' '}
+                        <Link to="#!" type="button" className="btn btn-secondary">Cancel</Link>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div >
         </div>
       </div>
     </div>
